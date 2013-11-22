b3ea16a517160268d55a815bae4221bfcf079a70
Jonathan Edgecombe <jpe505@york.ac.uk>
2013-11-22 15:06:44 +0000

    Add Runway.java.

---
+++ src/main/java/com/planepanic/game/model/Runway.java
@@ -0,0 +1,15 @@
+package com.planepanic.game.model;
+
+import lombok.Getter;
+
+public final class Runway extends Drawable {
+	@Getter private final String name;
+	@Getter private final double endX, endY;
+
+	public Runway(double startX, double startY, double endX, double endY, String name) {
+		super(startX, startY);
+		this.endX = endX;
+		this.endY = endY;
+		this.name = name;
+	}
+}
