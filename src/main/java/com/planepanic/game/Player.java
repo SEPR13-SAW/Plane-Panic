457ab448b28a813b9f8d9fbd3a8d5908f663d2a7
StevenWhitelock <sgw509@york.ac.uk>
2013-11-30 14:40:54 +0000

    Changed to use getters and setters

--- src/main/java/com/planepanic/game/Player.java
+++ src/main/java/com/planepanic/game/Player.java
@@ -1,22 +1,18 @@
 package com.planepanic.game;
 
+import lombok.Getter;
+import lombok.Setter;
+
+
+/**
+ * Player class containing name and score
+ * 
+ * @author Steven Whitelock
+ */
+
 public class Player {
 
-	/**
-	 * Player class containing name and score
-	 * 
-	 * @author Steven Whitelock
-	 */
+	@Getter @Setter public int score = 0;
+	@Getter @Setter public String playerName = "Doge";
 
-	public int score = 0;
-	public String playerName = "Doge";
-
-	public void changeScore(int newValue) {
-		this.score = newValue;
-	}
-
-	public void changeName(String newName) {
-		this.playerName = newName;
-
-	}
 }
