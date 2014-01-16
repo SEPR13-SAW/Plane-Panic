431ae040b26b2bdb4f1e85cd2359edffba8a0947
Ben Leyland <bl600@york.ac.uk>
2014-01-16 12:17:26 +0000

    Plane Limiting to Only 10

--- src/main/java/com/planepanic/game/gfx/screens/Game.java
+++ src/main/java/com/planepanic/game/gfx/screens/Game.java
@@ -118,7 +118,7 @@ public class Game extends Screen {
 	 * spawnInterval < maxSpawnInterval Spawns the first plane immediately
 	 */
 	public void spawnPlane() {
-		if (this.getTimer().getSeconds() % this.getSpawnInterval() == 0 && this.getTimer().getTicks() == 0) {
+		if (this.getTimer().getSeconds() % this.getSpawnInterval() == 0 && this.getTimer().getTicks() == 0 && planeList.size() < 10) {
 			int index = this.random.nextInt(this.entryPointList.size());
 			Plane plane = this.entryPointList.get(index).addPlane(this);
 			DrawThread draw = DrawThread.getInstance();
